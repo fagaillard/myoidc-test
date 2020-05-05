38b5f40d6fae7d76525ae2d549b5216b578189bb
fabrice gaillard <fabrice.gaillard@epfl.ch>
2020-05-05 10:36:34 +0200

    display http header in tornado

--- tornado/index.py
+++ tornado/index.py
@@ -8,13 +8,21 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         #return the ip address the the network inteface
         ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
-        self.write(ip)
+
+        html = "<html><head><title>Sample OIDC app</title></head><body>"
+        html = html + "<p>My Pod Ip address is " + ip + "</p>"
+        html = html + "<p>Avalaible HTTP Headers</p><pre>"
+        for key, value in self.request.headers.items():
+          html = html + key + " : " + value + "\n"
+        html = html + "</pre></body></html>"
+        self.write(html)
 
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
+
 
 if __name__ == "__main__":
     app = make_app()
